a32d8d66055196bb3c462c02db12a2361343961b
franck <carnet.franck.paul@gmail.com>
2025-06-27 13:20:59 +0200

    Add overload in define

--- _define.php
+++ _define.php
@@ -15,12 +15,13 @@ $this->registerModule(
     'NeoDark',
     'NeoDark theme for Dotclear 2 - design by https://lord.re/',
     'Franck Paul',
-    '6.0',
+    '5.3',
     [
         'date'     => '2025-05-15T18:46:46+0200',
         'requires' => [['core', '2.35']],
         'type'     => 'theme',
         'tplset'   => 'dotty',
+        'overload' => true,
 
         'details'    => 'https://open-time.net/?q=neodark',
         'support'    => 'https://github.com/franck-paul/neodark',
